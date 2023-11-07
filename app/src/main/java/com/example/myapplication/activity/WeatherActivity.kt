f57002f89bd10dfad4e3e8ea41ba489a767aaee9
0ccssu <128601631+0ccssu@users.noreply.github.com>
2023-11-07 20:50:17 +0900

    날씨 화면 일부 작업
    날씨 화면 검색바 뷰

--- app/src/main/java/com/example/myapplication/activity/WeatherActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/WeatherActivity.kt
@@ -1,4 +1,16 @@
 package com.example.myapplication.activity
 
-class WeatherActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.myapplication.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_weather)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
 }
